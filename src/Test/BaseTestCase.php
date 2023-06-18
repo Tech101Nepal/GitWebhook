3572fc6249fc472544a1bfa00e3ebbaf794b6bfd
Samir-Lama <samirlama2010@gmail.com>
2023-06-18 11:07:42 +0545

    setting default value for the config for the test

--- src/Test/BaseTestCase.php
+++ src/Test/BaseTestCase.php
@@ -2,11 +2,20 @@
 
 namespace Tech101\Webhook\Test;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Foundation\Testing\TestCase;
 use Tech101\Webhook\src\Test\CreatesApplication;
 
 class BaseTestCase extends \Orchestra\Testbench\TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Config::set('git.gitlabWebhookToken', 'token');
+        Config::set('git.defaultBranch', 'main');
+
+    }
     public function getProtectedMethod(string $className, string $methodName)
     {
         $class = new \ReflectionClass($className);
